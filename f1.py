632044553c7dff44e77d9b9951d9d8551ecdf4c2
Subham Kumar <55017512+sks-07@users.noreply.github.com>
2021-08-07 00:59:44 +0530

    Add files via upload

---
+++ f1.py
@@ -0,0 +1,40 @@
+import requests
+
+
+
+class formula1:
+    def __init__(self): 
+        self.payload = {}
+        self.headers = {}
+
+    def driverdatabase_season(self,year):
+        dirver_in_season = "http://ergast.com/api/f1/{}/drivers".format(year)+".json"
+        response = requests.request("GET", dirver_in_season, headers=self.headers, data=self.payload)
+        database = eval(response.text)
+        return database['MRData']['DriverTable']
+
+    def display(self,year):
+        db=self.driverdatabase_season(year)
+        print("Drivers in Formula 1 season 2021:\n")
+        for key in db['Drivers']:
+            try:
+                if int(key['permanentNumber'])<10:
+                    print(" "+key['permanentNumber']+"  "+ key['code'] +":  "+key['givenName'] + " " + key['familyName'])
+                else:
+                    print(key['permanentNumber']+"  "+ key['code'] +":  "+key['givenName'] + " " + key['familyName'])
+            except:
+                print("NA"+"  "+ key['code'] +":  "+key['givenName'] + " " + key['familyName'])
+
+f=formula1()
+f.display(2010)
+
+        
+
+    
+
+
+    
+
+
+
+
