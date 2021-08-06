d183cc3bc72f5415bdc0320b915ba279f6b3212e
sks-07 <subhamks010@gmail.com>
2021-08-07 03:18:51 +0530

    Update f1.py

--- f1.py
+++ f1.py
@@ -25,6 +25,29 @@ class formula1:
             except:
                 print("NA"+"  "+ key['code'] +":  "+key['givenName'] + " " + key['familyName'])
 
+    def last_result(self):
+        url = "http://ergast.com/api/f1/current/last/results.json"
+        response = requests.request("GET", url, headers=self.headers, data=self.payload)
+        db = eval(response.text)
+        db=db['MRData']['RaceTable']['Races'][0]
+        print("\nRound {} Forumla 1 season {} :".format(db['round'],db['season']))
+        print(db['raceName'])
+        print("At circuit {} ,{} {}".format(db['Circuit']['circuitName'],db['Circuit']['Location']['locality'],db['Circuit']['Location']['country']))
+        print("Date: {}  Time: {}".format(db['date'],db['time']))
+        print("Pos\tNo\tDriver\t\t\tLaps\tPoints")
+        for key in db['Results']:
+            try:
+            except:
+                if key['status']=="Disqualified":
+                    status="DQ"
+                elif key['status']=='Collision':
+                    status="DNF"
+                else:
+                    status=key['status']
+                print("{}\t{}\t{}\t\t{}\t{}".format(key['position'],key['number'],key['Driver']['givenName']+" " +key['Driver']['familyName'],key['laps'],status,key['points'] ))
+
+
+
 f=formula1()
 f.last_result()
 
